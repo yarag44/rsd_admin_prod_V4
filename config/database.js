dd0c1df0edbf5571b25daae91698feded4a4a34d
Alberto GAray <yarag44@hotmail.com>
2022-08-16 21:04:03 -0700

    Adjust Connectoins

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
     },
     pool: {
           min: 0,
-          max: 4,
+          max: 2,
           idleTimeoutMillis: 30000000,
           createTimeoutMillis: 30000000,
           acquireTimeoutMillis: 30000000,
